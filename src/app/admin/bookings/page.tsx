2850d37b0d3f2351a4c75265c49d4daa63ae382b
FiandraLazuart <v3346589@gmail.com>
2026-08-12 22:48:27 +0700

    feat: fix suspence booking

--- src/app/admin/bookings/page.tsx
+++ src/app/admin/bookings/page.tsx
@@ -1,11 +1,13 @@
 import { Metadata } from "next";
 import BookingView from "@/components/view/Admin/Booking";
-
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
 	title: "Bookings",
 };
 
 export default function BuildingPage() {
-	return <BookingView />
+	<Suspense fallback={null}>
+		return <BookingView />
+	</Suspense>;
 }
